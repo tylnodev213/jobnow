9978be6b81f34e1f836ceade5cd3d29184b3ee20
duonght213 <duonght.paraline@gmail.com>
2022-12-14 20:53:26 +0700

    master updated bootstrap 5

--- resources/views/web/layouts/master.blade.php
+++ resources/views/web/layouts/master.blade.php
@@ -9,7 +9,7 @@
     <title>@yield('title', config('app.name', '@Master Layout'))</title>
 
     {{--Styles css common--}}
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @yield('style-libraries')
     {{--Styles custom--}}
@@ -26,6 +26,8 @@
 <script src="{{ asset('js/jquery-3.4.1.js') }}"></script>
 {{--Scripts link to file or js custom--}}
 @yield('scripts')
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+@stop
 </body>
 </html>
 
