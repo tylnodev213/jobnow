ef07a2bb4be604d69c785b9f757a21efc499f726
michael <haminhchi18082002@gmail.com>
2022-12-16 18:49:02 +0700

    change 16/12/2022

--- resources/views/web/element/header.blade.php
+++ resources/views/web/element/header.blade.php
@@ -1,31 +1,39 @@
 <section class="f-s-20"
          style="box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;">
-    <div class="container">
-        <div class="row my-3 justify-content-between mx-0 align-items-center">
-            <div class="col-3">
-                <img class="w-100" src="\assets\img\img-logo.jpg" alt="">
-            </div>
-            <div class="menu-link col-xl-5 col-6 d-flex justify-content-around text-decoration-none text-black">
-                <a class="text-decoration-none text-black" href="">
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img class="w-100" src="/img/img-logo.png" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-around" id="navbarNavAltMarkup">
+                <a class="nav-link text-decoration-none text-black" href="">
                     Home
                 </a>
-                <a class="text-decoration-none text-black" href="">
+                <a class="nav-link text-decoration-none text-black" href="">
                     Find job
                 </a>
-                <a class="text-decoration-none text-black" href="">
+                <a class="nav-link text-decoration-none text-black" href="">
                     Find Candidates
                 </a>
-                <a class="text-decoration-none text-black" href="">
+                <a class="nav-link text-decoration-none text-black" href="">
                     Article
                 </a>
+                <div class="nav-link">
+                    <div class="btn ps-0 f-s-20">Login</div>
+                    <button class="py-md-1 px-md-3 py-2 px-4 text-white bg-purble "
+                            style="border-radius: 10px;max-width: 167px; border: none;">
+                        Register now
+                    </button>
+                </div>
+
             </div>
-            <div class="col-1 d-flex justify-content-center d-f">
-                Login
-            </div>
-            <button class="col-2  py-2 text-white d-flex justify-content-center bg-purble "
-                    style="border-radius: 10px;max-width: 167px; border: none;">
-                Register now
-            </button>
+
         </div>
-    </div>
+    </nav>
+
 </section>
