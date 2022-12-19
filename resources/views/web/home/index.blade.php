d63932bca91e915184983869839f4e173fcb2b6d
michael <haminhchi18082002@gmail.com>
2022-12-19 20:56:29 +0700

    change 19/12/2022 version2

--- resources/views/web/home/index.blade.php
+++ resources/views/web/home/index.blade.php
@@ -5,7 +5,30 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css">
     <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
-@stop
+<style>
+    @media screen and (max-width:991px) {
+    .input-search-btn{
+        top: 115%;
+         right: 39%;
+         height: 40px !important;
+    }
+    .input-search{
+        border-radius: 20px !important;
+    }
+    .input-search0{
+        border-radius: 20px !important;
+border-right: none !important;
+    }
+}
+
+ @media screen and (min-width:992px) {
+    .input-search-btn{
+        top: 0%;
+         right: 0%;
+    }
+ }
+</style>
+    @stop
 
 @section('content')
     <section>
@@ -18,15 +41,15 @@
             <div class="row my-2 my-md-3 my-lg-5 f-s-18 text-gray justify-content-center">
                 <div class="col-lg-4 col-10 my-lg-0 my-2 position-relative  px-0">
                     <input class="w-100 ps-5 input-search0 " placeholder=" Search Title or Keyword" type="text"
-                           style="border-radius: 20px 0 0 20px;height: 60px; border: none;border-right: 2px solid #878787 ;box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;">
+                        style="border-radius: 20px 0 0 20px;height: 60px; border: none;border-right: 2px solid #878787 ;box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;">
                     <i class="fa fa-search position-absolute f-s-30" style="top:26%; left:3%"></i>
                 </div>
                 <div class="col-lg-6 col-10 position-relative  px-0 mb-lg-0 mb-5">
                     <input type="text" placeholder="Search Location" class="w-100 ps-5 input-search"
-                           style="border-radius: 0 20px 20px 0px; height: 60px; border: none;box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;">
+                        style="border-radius: 0 20px 20px 0px; height: 60px; border: none;box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;">
                     <i class="fa fa-location-dot position-absolute f-s-30" style="top: 26%; left: 3%;"></i>
                     <button class="text-white bg-purble position-absolute input-search-btn py-2"
-                            style="width: 119px; height: 60px;  border-radius: 20px; border: none;">search</button>
+                        style="width: 119px; height: 60px;  border-radius: 20px; border: none;">search</button>
                 </div>
             </div>
         </div>
@@ -38,7 +61,7 @@
                     Featured Job Circulars
                 </div>
             </div>
-            <div class="row row-cols-4">
+            <div class="row row-cols-lg-4 row-cols-1 row-cols-sm-2">
                 <div class="col my-4 ">
                     <div class="card " style="border: none; border-radius:15px ;  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;">
                         <div class="row mt-3 mx-0 align-items-center">
@@ -57,7 +80,7 @@
                                 additional content. This content is a little bit longer.</p>
                             <div class="row">
                                 <span class="f-s-12 col"><span class="f-s-18" >$2500</span>/month</span>
-                                <a href="#" class="f-s-18 text-decoration-none  col text-purple" >Apply now</a>
+                                <a href="#" class="f-s-18 text-decoration-none  col text-purple text-end" >Apply now</a>
                             </div>
                         </div>
                         <div class="card-footer " style=" border-top:none">
