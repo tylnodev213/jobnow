7a620aa338b46a0431d2bfe5c7329dd30bc57a1a
michael <haminhchi18082002@gmail.com>
2022-12-19 19:12:31 +0700

    change 19/12/2022

--- resources/views/web/home/index.blade.php
+++ resources/views/web/home/index.blade.php
@@ -4,6 +4,7 @@
 @section('style-libraries')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
 @stop
 
 @section('content')
@@ -39,7 +40,33 @@
             </div>
             <div class="row row-cols-4">
                 <div class="col my-4 ">
-                    <div class="card " style="border: none; box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;">
+                    <div class="card " style="border: none; border-radius:15px ;  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;">
+                        <div class="row mt-3 mx-0 align-items-center">
+                            <div class="col-3">
+                                <img  src="assets/img/logo-company.png" class="card-img-top w-100" alt="...">
+                            </div>
+                            <div class="col d-flex flex-column f-s-18 text-start">
+                                Microsoft
+                                <span class="f-s-12 ">Freelancer</span>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Senior UI Designer</h5>
+                            <p class="f-s-12">Fulltime</p>
+                            <p class="card-text text-justify text-space">This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.</p>
+                            <div class="row">
+                                <span class="f-s-12 col"><span class="f-s-18" >$2500</span>/month</span>
+                                <a href="#" class="f-s-18 text-decoration-none  col text-purple" >Apply now</a>
+                            </div>
+                        </div>
+                        <div class="card-footer " style=" border-top:none">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col my-4 ">
+                    <div class="card " style="border: none; box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;">
                         <img src="..." class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
@@ -52,7 +79,7 @@
                     </div>
                 </div>
                 <div class="col my-4 ">
-                    <div class="card " style="border: none; box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;">
+                    <div class="card " style="border: none; box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;">
                         <img src="..." class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
@@ -65,20 +92,7 @@
                     </div>
                 </div>
                 <div class="col my-4 ">
-                    <div class="card " style="border: none; box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">Last updated 3 mins ago</small>
-                        </div>
-                    </div>
-                </div>
-                <div class="col my-4 ">
-                    <div class="card " style="border: none; box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;">
+                    <div class="card " style="border: none; box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;">
                         <img src="..." class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
@@ -92,7 +106,7 @@
                 </div>
 
                 <div class="col my-4 ">
-                    <div class="card " style="border: none; box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;">
+                    <div class="card " style="border: none; box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;">
                         <img src="..." class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
